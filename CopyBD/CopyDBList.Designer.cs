4b60dd26b0cb6eea0e20bd9ed11e145f6a850d39
evkuz8 <@106-05>
2018-06-08 12:53:09 +0500

    eda

---
+++ CopyBD/CopyDBList.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CopyBD
+{
+    partial class CopyDBList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxBDOnSrvr = new System.Windows.Forms.ListBox();
+            this.listBoxBDForCopy = new System.Windows.Forms.ListBox();
+            this.SrvrNameLbl = new System.Windows.Forms.Label();
+            this.BdOnSrvrLbl = new System.Windows.Forms.Label();
+            this.ToBtn = new System.Windows.Forms.Button();
+            this.refreshBDListBtn = new System.Windows.Forms.Button();
+            this.textBoxSrvrName = new System.Windows.Forms.TextBox();
+            this.BdForCopyLbl = new System.Windows.Forms.Label();
+            this.delBtn = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.postfixTxt = new System.Windows.Forms.TextBox();
+            this.postfixLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxBDOnSrvr
+            // 
+            this.listBoxBDOnSrvr.FormattingEnabled = true;
+            this.listBoxBDOnSrvr.Location = new System.Drawing.Point(12, 105);
+            this.listBoxBDOnSrvr.Name = "listBoxBDOnSrvr";
+            this.listBoxBDOnSrvr.Size = new System.Drawing.Size(188, 225);
+            this.listBoxBDOnSrvr.TabIndex = 0;
+            this.listBoxBDOnSrvr.SelectedIndexChanged += new System.EventHandler(this.listBoxBDOnSrvr_SelectedIndexChanged);
+            // 
+            // listBoxBDForCopy
+            // 
+            this.listBoxBDForCopy.FormattingEnabled = true;
+            this.listBoxBDForCopy.Location = new System.Drawing.Point(249, 105);
+            this.listBoxBDForCopy.Name = "listBoxBDForCopy";
+            this.listBoxBDForCopy.Size = new System.Drawing.Size(205, 225);
+            this.listBoxBDForCopy.TabIndex = 1;
+            // 
+            // SrvrNameLbl
+            // 
+            this.SrvrNameLbl.AutoSize = true;
+            this.SrvrNameLbl.Location = new System.Drawing.Point(12, 41);
+            this.SrvrNameLbl.Name = "SrvrNameLbl";
+            this.SrvrNameLbl.Size = new System.Drawing.Size(77, 13);
+            this.SrvrNameLbl.TabIndex = 2;
+            this.SrvrNameLbl.Text = "Имя сервера:";
+            // 
+            // BdOnSrvrLbl
+            // 
+            this.BdOnSrvrLbl.AutoSize = true;
+            this.BdOnSrvrLbl.Location = new System.Drawing.Point(9, 89);
+            this.BdOnSrvrLbl.Name = "BdOnSrvrLbl";
+            this.BdOnSrvrLbl.Size = new System.Drawing.Size(125, 13);
+            this.BdOnSrvrLbl.TabIndex = 3;
+            this.BdOnSrvrLbl.Text = "Список баз на сервере";
+            // 
+            // ToBtn
+            // 
+            this.ToBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ToBtn.Location = new System.Drawing.Point(206, 172);
+            this.ToBtn.Name = "ToBtn";
+            this.ToBtn.Size = new System.Drawing.Size(37, 23);
+            this.ToBtn.TabIndex = 4;
+            this.ToBtn.Text = "-->";
+            this.ToBtn.UseVisualStyleBackColor = true;
+            this.ToBtn.Click += new System.EventHandler(this.ToBtn_Click);
+            // 
+            // refreshBDListBtn
+            // 
+            this.refreshBDListBtn.Location = new System.Drawing.Point(216, 36);
+            this.refreshBDListBtn.Name = "refreshBDListBtn";
+            this.refreshBDListBtn.Size = new System.Drawing.Size(143, 23);
+            this.refreshBDListBtn.TabIndex = 5;
+            this.refreshBDListBtn.Text = "Обновить список БД";
+            this.refreshBDListBtn.UseVisualStyleBackColor = true;
+            this.refreshBDListBtn.Click += new System.EventHandler(this.refreshBDListBtn_Click);
+            // 
+            // textBoxSrvrName
+            // 
+            this.textBoxSrvrName.Location = new System.Drawing.Point(100, 38);
+            this.textBoxSrvrName.Name = "textBoxSrvrName";
+            this.textBoxSrvrName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSrvrName.TabIndex = 6;
+            // 
+            // BdForCopyLbl
+            // 
+            this.BdForCopyLbl.AutoSize = true;
+            this.BdForCopyLbl.Location = new System.Drawing.Point(246, 89);
+            this.BdForCopyLbl.Name = "BdForCopyLbl";
+            this.BdForCopyLbl.Size = new System.Drawing.Size(155, 13);
+            this.BdForCopyLbl.TabIndex = 7;
+            this.BdForCopyLbl.Text = "Список баз для копирования";
+            // 
+            // delBtn
+            // 
+            this.delBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.delBtn.Location = new System.Drawing.Point(206, 216);
+            this.delBtn.Name = "delBtn";
+            this.delBtn.Size = new System.Drawing.Size(37, 23);
+            this.delBtn.TabIndex = 8;
+            this.delBtn.Text = "<--";
+            this.delBtn.UseVisualStyleBackColor = true;
+            this.delBtn.Click += new System.EventHandler(this.delBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(355, 346);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(99, 23);
+            this.startBtn.TabIndex = 9;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // postfixTxt
+            // 
+            this.postfixTxt.Location = new System.Drawing.Point(249, 348);
+            this.postfixTxt.Name = "postfixTxt";
+            this.postfixTxt.Size = new System.Drawing.Size(100, 20);
+            this.postfixTxt.TabIndex = 11;
+            // 
+            // postfixLbl
+            // 
+            this.postfixLbl.AutoSize = true;
+            this.postfixLbl.Location = new System.Drawing.Point(182, 351);
+            this.postfixLbl.Name = "postfixLbl";
+            this.postfixLbl.Size = new System.Drawing.Size(61, 13);
+            this.postfixLbl.TabIndex = 10;
+            this.postfixLbl.Text = "Постфикс:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(12, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "БазаДанных42";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(92, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "*постфикс*";
+            // 
+            // CopyDBList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 391);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.postfixTxt);
+            this.Controls.Add(this.postfixLbl);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.delBtn);
+            this.Controls.Add(this.BdForCopyLbl);
+            this.Controls.Add(this.textBoxSrvrName);
+            this.Controls.Add(this.refreshBDListBtn);
+            this.Controls.Add(this.ToBtn);
+            this.Controls.Add(this.BdOnSrvrLbl);
+            this.Controls.Add(this.SrvrNameLbl);
+            this.Controls.Add(this.listBoxBDForCopy);
+            this.Controls.Add(this.listBoxBDOnSrvr);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "CopyDBList";
+            this.Text = "Копирование Баз Данных";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxBDOnSrvr;
+        private System.Windows.Forms.ListBox listBoxBDForCopy;
+        private System.Windows.Forms.Label SrvrNameLbl;
+        private System.Windows.Forms.Label BdOnSrvrLbl;
+        private System.Windows.Forms.Button ToBtn;
+        private System.Windows.Forms.Button refreshBDListBtn;
+        private System.Windows.Forms.TextBox textBoxSrvrName;
+        private System.Windows.Forms.Label BdForCopyLbl;
+        private System.Windows.Forms.Button delBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox postfixTxt;
+        private System.Windows.Forms.Label postfixLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ CopyBD/CopyDBList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CopyBD
+{
+    public partial class CopyDBList : Form
+    {
+        public CopyDBList()
+        {
+            InitializeComponent();
+        }
+
+        private void refreshBDListBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(textBoxSrvrName.Text.Length!=0)
+                { 
+                    listBoxBDForCopy.Items.Clear();
+                    listBoxBDOnSrvr.Items.Clear();
+
+                    SqlConnection refreshCon = new SqlConnection($@"Data Source = {textBoxSrvrName.Text}; Initial Catalog = master; Integrated Security = true;");
+                    SqlCommand refreshCmd = new SqlCommand("SELECT * FROM sysdatabases WHERE name NOT IN ('master','tempdb','model','msdb','ReportServer$MSSQL','ReportServer$MSSQLTempDB') ORDER BY name", refreshCon);
+                    refreshCon.Open();
+
+                    SqlDataReader reader = refreshCmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        listBoxBDOnSrvr.Items.Add(reader[0]);
+                    }
+                
+                    reader.Close();
+                    refreshCon.Close();
+                }
+                else
+                {
+                MessageBox.Show("Пожалуйста, заполните имя сервера");
+            }
+        }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Сервер с таким именем не существует или недоступен!");
+            }
+        }
+
+        private void ToBtn_Click(object sender, EventArgs e)
+        {
+            if (listBoxBDOnSrvr.SelectedItem!=null)
+            {
+                int selectedindex = listBoxBDOnSrvr.SelectedIndex;
+                var selected = listBoxBDOnSrvr.SelectedItem;
+                listBoxBDForCopy.Items.Add(selected);
+                listBoxBDOnSrvr.Items.Remove(selected);
+                if (listBoxBDOnSrvr.Items.Count > 0)
+                {
+                    if (selectedindex != listBoxBDOnSrvr.Items.Count )
+                    {
+                        listBoxBDOnSrvr.SetSelected(selectedindex, true);
+
+                    }
+                    else
+                    {
+                        listBoxBDOnSrvr.SetSelected(--selectedindex, true);
+                    }
+                }
+            }
+
+        }
+
+        private void listBoxBDOnSrvr_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            if (listBoxBDForCopy.SelectedItem!= null)
+            {
+                int selectedindex = listBoxBDForCopy.SelectedIndex;
+
+                var selected = listBoxBDForCopy.SelectedItem;
+                listBoxBDOnSrvr.Items.Add(selected);
+                listBoxBDForCopy.Items.Remove(selected);
+                if (listBoxBDForCopy.Items.Count > 0)
+                {
+                    if (selectedindex != listBoxBDForCopy.Items.Count)
+                    {
+                        listBoxBDForCopy.SetSelected(selectedindex, true);
+
+                    }
+                    else
+                    {
+                        listBoxBDForCopy.SetSelected(--selectedindex, true);
+                    }
+                }
+
+            }
+            
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxSrvrName.Text.Length != 0)
+                {
+                    if (postfixTxt.Text.Length !=0 )
+                    {
+                        foreach (var item in listBoxBDForCopy.Items)
+                        {
+                            SqlConnection connection = new SqlConnection($"Data Source = {textBoxSrvrName.Text}; Initial Catalog = {item.ToString()}; Integrated Security = true");
+
+                            connection.Open();
+
+                            SqlCommand backup = new SqlCommand(
+                                $@"USE master BACKUP DATABASE [{item.ToString()}] TO  DISK = N'D:\Microsoft SQL Server\MSSQL13.MSSQL\MSSQL\Backup\{item.ToString()}.bak' WITH NOFORMAT, NOINIT,  NAME = N'{item.ToString()}-Полная База данных Резервное копирование', SKIP, NOREWIND, NOUNLOAD,  STATS = 10",
+                                connection);
+                            backup.ExecuteNonQuery();
+
+                            SqlCommand restore = new SqlCommand(
+                                $@"USE master RESTORE DATABASE [{item.ToString() + postfixTxt.Text}] FROM DISK = N'D:\Microsoft SQL Server\MSSQL13.MSSQL\MSSQL\Backup\{item.ToString()}.bak' WITH  MOVE N'{item.ToString()}' TO N'D:\Microsoft SQL Server\MSSQL13.MSSQL\MSSQL\DATA\{item.ToString() + postfixTxt.Text}.mdf',  MOVE N'{item.ToString()}_log' TO N'D:\Microsoft SQL Server\MSSQL13.MSSQL\MSSQL\DATA\{item.ToString() + postfixTxt.Text}.ldf',  NOUNLOAD,  STATS = 5",
+                                connection);
+                            restore.ExecuteNonQuery();
+
+                            connection.Close();
+                        }
+                        listBoxBDForCopy.Items.Clear();
+                        MessageBox.Show("Готово!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пожалуйста, введите постфикс");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, заполните имя сервера");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+                //MessageBox.Show("Сервер с таким именем не существует или недоступен!");
+            }
+
+        }
+    }
+}
